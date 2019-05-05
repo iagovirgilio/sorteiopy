f502966905111a8209b8300d0738e9f8bf2e3958
Iago Virgílio <iagovirgilio@gmail.com>
2019-05-05 13:27:02 -0300

    Adicionado app django pwa

--- sorteador/settings.py
+++ sorteador/settings.py
@@ -15,8 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'sort/static/js', 'serviceworker.js')
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
